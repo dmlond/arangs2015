cd879357ba0492f6a21f5e1c21fcb994eeb2ceb9
Rutger Vos <rutgeraldo@gmail.com>
2015-05-09 13:57:17 +0200

    added comment

--- bin/download_bwa.sh
+++ bin/download_bwa.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# this script downloads the BWA programming for mapping short reads to a reference
 wget -O bwa-0.7.12.tar.bz2 http://sourceforge.net/projects/bio-bwa/files/bwa-0.7.12.tar.bz2/download
