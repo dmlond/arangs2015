b35e709dd3ef434e4341120ba4813478b591c6c3
dmlond <dmlond@gmail.com>
2015-05-13 13:57:29 +0100

    added pipeline. NOTE: experimental

--- conf/vagrant/manifests/default.pp
+++ conf/vagrant/manifests/default.pp
@@ -98,6 +98,48 @@ exec {
 	'symlink_data':
 		command   => 'ln -s /vagrant_data /home/vagrant/arangs2015/data',
 		require   => Exec[ 'rm_repo_data' ];
-		
 
+    # the pipeline itself. whether or not it is actually a good idea to 
+    # run the pipeline during provisioning is up for debate, but just to
+    # demonstrate that this is possible, here it is. Note that we are now
+    # introducing a number of variables (starting with $), that have to 
+    # be made available by 'facter'. In the current setup, the values of
+    # these variables are defined in the Vagrantfile. There are other ways
+    # to make them available as well, such as from the environment, among
+    # others.
+    'index_reference':
+        command   => "bwa index -a bwtsw ${reference}",
+        creates   => "/home/vagrant/arangs2015/data/${reference}.bwt",
+        cwd       => "/home/vagrant/arangs2015/data",
+        require   => Exec[ 'symlink_bwa', 'symlink_data' ];
+    'map_r1':
+        command   => "bwa aln -t ${cores} ${reference} ${fastq1} -f ${fastq1}.sai",
+        creates   => "/home/vagrant/arangs2015/data/${fastq1}.sai",
+        cwd       => "/home/vagrant/arangs2015/data",    
+        require   => Exec[ 'index_reference' ];
+    'map_r2':
+        command   => "bwa aln -t ${cores} ${reference} ${fastq2} -f ${fastq2}.sai",
+        creates   => "/home/vagrant/arangs2015/data/${fastq2}.sai",
+        cwd       => "/home/vagrant/arangs2015/data",    
+        require   => Exec[ 'index_reference' ];
+    'bwa_sampe':
+        command   => "bwa sampe ${reference} ${fastq1}.sai ${fastq2}.sai ${fastq1} ${fastq2} -f ${sam}",
+        creates   => "/home/vagrant/arangs2015/data/${sam}",
+        cwd       => "/home/vagrant/arangs2015/data",
+        require   => Exec[ 'map_r1', 'map_r2' ];
+	'samtools_filter':
+        command   => "samtools view -bS -F 4 -q 50 -o ${sam}.filtered ${sam}",
+        creates   => "/home/vagrant/arangs2015/data/${sam}.filtered",
+        cwd       => "/home/vagrant/arangs2015/data",
+        require   => Exec[ 'bwa_sampe', 'symlink_samtools' ];
+    'samtools_sort':
+        command   => "samtools sort ${sam}.filtered ${sam}.sorted",
+        creates   => "/home/vagrant/arangs2015/data/${sam}.sorted.bam",
+        cwd       => "/home/vagrant/arangs2015/data",
+        require   => Exec[ 'samtools_filter' ];
+    'samtools_index':
+        command   => "samtools index ${sam}.sorted.bam",
+        creates   => "/home/vagrant/arangs2015/data/${sam}.sorted.bam.bai",
+        cwd       => "/home/vagrant/arangs2015/data",
+        require   => Exec[ 'samtools_sort' ];        
 }
